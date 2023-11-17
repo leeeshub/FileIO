fcdbb9bc0bfed2c115896ed1c4d0c2cb3461e723
Hyoin Lee <hiliee09@naver.com>
2023-11-17 21:07:35 +0900

    modified code

--- app/src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ app/src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -7,33 +7,35 @@ import java.io.*;
  */
 public class UsingBufferedReader {
     public static void main(String[] args) throws IOException {
-
+    	String filename = args[0];
 
         //-------------- Test reading 1 MB file. --------------------
+    	if(filename.equals("input1MB.txt")) {
+    		StopWatch.start();
 
-        StopWatch.start();
+            BufferedReader inputStream= new BufferedReader(new FileReader(filename));
+            while (inputStream.read()!=-1){}
 
-        BufferedReader inputStream= new BufferedReader(new FileReader(DumpDataWriter.input1MB));
-        while (inputStream.read()!=-1){}
-
-        long duration = StopWatch.stop();
-        System.out.println(duration + " milsec");
-        
-        inputStream.close();
-
+            long duration = StopWatch.stop();
+            System.out.println(duration + " milsec");
+            
+            inputStream.close();
+    	}
 
         //-------------- Test reading 10 MB file. --------------------
 
-        StopWatch.start();
-
-        BufferedReader inputStream2= new BufferedReader(new FileReader(DumpDataWriter.input10MB));
-        while (inputStream2.read()!=-1){}
-
-        long duration2 = StopWatch.stop();
-        System.out.println(duration2 + " milsec");
-
-        inputStream2.close();
-
+    	if(filename.equals("input10MB.txt")) {
+	    	StopWatch.start();
+	
+	        BufferedReader inputStream2= new BufferedReader(new FileReader(filename));
+	        while (inputStream2.read()!=-1){}
+	
+	        long duration2 = StopWatch.stop();
+	        System.out.println(duration2 + " milsec");
+	
+	        inputStream2.close();
+    	}
+    	
         /*
         //-------------- Test reading 100 MB file. --------------------
 
